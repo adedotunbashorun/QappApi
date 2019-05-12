fae666683d3902165bd3bc9774ebc85cceab95d1
adedotun <adedotunolawale@gmail.com>
2019-05-12 16:53:40 +0100

    worked on url for main app

--- Modules/User/Seeders/UserSeeder.js
+++ Modules/User/Seeders/UserSeeder.js
@@ -14,25 +14,10 @@ UserSeeder.seedUser = (req, res) => {
         user.first_name = 'Dev'
         user.last_name = 'Admin'
         user.email = 'adedotunolawale@gmail.com'
-        user.temporarytoken = crypto.randomBytes(20).toString('hex')
+        user.temporarytoken = null
         user.password = User.hashPassword('123456')
+        users.is_active = true
         user.save()
-
-        User.findOne({ email: 'adedotunolawale@gmail.com' }, function(error, users) {
-            if (error) {
-                res.json(error)
-            } else if (!users) {
-                res.json({ 'msg': 'token do not math' })
-            } else {
-                users.temporarytoken = null
-                users.is_active = true
-                // Activity.Email(user, 'Account Activated', 'Hello ' + user.first_name + ' ' + user.last_name + ', Thank you for registering at Brax Map. Your Account has been activated successfully.')
-                //getting mail data
-                users.save()
-                return res.status(201).json(users)
-            }
-
-        })
 
         // seeded!
         return res.status(201).json({ msg: 'User Seeded', user: user })
