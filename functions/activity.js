8da63764e6b905add5d2dedc5ee49e3413346fc2
user.name : adedotunbashorun <user.email : adedotunolawale@gmail.com>
2019-07-22 11:38:29 +0100

    ..

--- functions/activity.js
+++ functions/activity.js
@@ -27,8 +27,8 @@ let options2 = {
     port: 465, // Port
     secure: true, // this is true as port is 465
     auth: {
-        user: config.GMAIL_USERNAME, //Gmail username
-        pass: config.GMAIL_PASSWORD // Gmail password
+        user: "adedotunolawale@gmail.com", //Gmail username
+        pass: "professionals@2014" // Gmail password
     }
 }
 
